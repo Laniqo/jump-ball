1717ba91a7c8d33aef9a714b66161c5915a68753
undefxned <undefxned.variable>
2017-12-11 04:28:55 +0200

    all files
    uploaded all files

---
+++ Assets/Grav.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grav : MonoBehaviour {
+
+	public float grav;
+
+	void Start(){
+
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.up * -grav;
+
+
+	}	
+}
